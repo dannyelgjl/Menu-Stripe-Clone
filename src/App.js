0e285ccc15dd4a009822a2517fd7a9b9e75fc40a
dannyelgjl <dannyelgjl@gmail.com>
2020-07-29 12:23:36 -0300

    created Layout Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 import GlobalStyles from "./styles/GlobalStyles";
+import Layout from "./components/Layout";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Layout>
+        <h1>teste</h1>
+      </Layout>
+
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
