2755a5477b6e8712aafed9e0a63016bbcac35e7e
dannyelgjl <dannyelgjl@gmail.com>
2020-07-29 12:34:00 -0300

    created Dropdown component

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Container } from "./styles";
+import { DropdownOption } from "../Dropdown";
+
+function NavBar() {
+  return (
+    <Container>
+      <ul>
+        <li>
+          <DropdownOption name="Produtos" content={() => <h1>Produtos</h1>} />
+        </li>
+
+        <li>
+          <DropdownOption
+            name="Desenvolvedores"
+            content={<h1>Desenvolvedores</h1>}
+          />
+        </li>
+
+        <li>
+          <DropdownOption name="Empresas" content={<h1>Empresas</h1>} />
+        </li>
+      </ul>
+    </Container>
+  );
+}
+
+export default NavBar;
--- src/components/NavBar/styles.js
+++ src/components/NavBar/styles.js
@@ -1,28 +1,3 @@
-import React from "react";
+import styled from "styled-components";
 
-import { Container } from "./styles";
-
-function NavBar() {
-  return (
-    <Container>
-      <ul>
-        <li>
-          <DropdownOption name="Produtos" content={<h1>Produtos</h1>} />
-        </li>
-
-        <li>
-          <DropdownOption
-            name="Desenvolvedores"
-            content={<h1>Desenvolvedores</h1>}
-          />
-        </li>
-
-        <li>
-          <DropdownOption name="Empresas" content={<h1>Empresas</h1>} />
-        </li>
-      </ul>
-    </Container>
-  );
-}
-
-export default NavBar;
+export const Container = styled.div``;
